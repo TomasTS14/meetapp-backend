05980c752b0e3a580b760f3a41eb7f9926fa761d
Tomas Casa WIn <210099@fppiramide.com>
2023-06-09 19:40:50 +0200

    deleting all test related for upload

--- dist/src/Server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = require("./App");
-const port = process.env.PORT;
-App_1.app.listen(process.env.PORT, () => {
-    console.log(`Application started on port ${port}`);
-});
